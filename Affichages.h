ec58a0f5eee5188413ca335409e5ee276452963b
eyaCherni965 <eya.cherni.1@ens.etsmtl.ca>
2025-08-21 23:11:51 -0400

    Update Affichages.h

--- Affichages.h
+++ Affichages.h
@@ -1,13 +1,10 @@
 /*******************************************************************************
-    AFFICHAGES.h  (version pour le TP2, A24)  *** Ce module est DONNÉ aux élèves
-    Auteurs : E.Thé
-
     Module qui contient les fonctions de gestion de l'affichage des tris en
     mode graphique avec des pixels. Offre aussi 3 petites fonctions utilitaires.
 ********************************************************************************/
 #if !defined (NOIR)
 
-// Les 3 couleurs à utiliser
+// Les 3 couleurs Ã  utiliser
 #define NOIR  0
 #define JAUNE 14
 #define BLANC 15
@@ -17,45 +14,45 @@
 int ouvrir_mode_graphique(void);
 
 /* Cette fonction permet de terminer le mode graphique.				 */
-/* ATTENTION : Le mode graphique doit avoir été ouvert.				 */
+/* ATTENTION : Le mode graphique doit avoir Ã©tÃ© ouvert.				 */
 void fermer_mode_graphique(void);
 
-/* Permet d'afficher un message "mess" dans le haut de l'écran.      */
-/* PARAMÈTRE: mess - la chaine de caractères à affciher.             */
+/* Permet d'afficher un message "mess" dans le haut de l'Ã©cran.      */
+/* PARAMÃˆTRE: mess - la chaine de caractÃ¨res Ã  affciher.             */
 void afficher_message(const char * mess);
 
 /* Cette fonction affiche le cadre d'un tri au depart, avec le titre */
-/* et le nombre d'opérations affiché en bas du cadre #indice.        */
+/* et le nombre d'opÃ©rations affichÃ© en bas du cadre #indice.        */
 /* Elle suppose que l'indice du tri est entre 0 et NB_TRIS - 1.		 */
-/* PARAMÈTRE: indice - le numéro du tri à afficher (0 à NB_TRIS-1).  */
-/*            nom_tri - le nom du tri à afficher au dessus du cadre. */
-/*            taille - le nombre total d'éléments à trier.           */
-/*            nb_oper - nombre total d'opérations pour faire ce tri. */
+/* PARAMÃˆTRE: indice - le numÃ©ro du tri Ã  afficher (0 Ã  NB_TRIS-1).  */
+/*            nom_tri - le nom du tri Ã  afficher au dessus du cadre. */
+/*            taille - le nombre total d'Ã©lÃ©ments Ã  trier.           */
+/*            nb_oper - nombre total d'opÃ©rations pour faire ce tri. */
 /* ATTENTION : Le mode graphique doit avoir ete ouvert.				 */
 /* ATTENTION : Elle supporte AU MAXIMUM 6 tris (indice < 6).    	 */
 void afficher_cadre_tri(int indice, const char * nom_tri, int taille, int nb_oper);
 
 
-/* Cette fonction affiche un point avec la "couleur" désirée pour la */
-/* "valeur" à la position de tableau "[pos]" du tri #indice_tri.     */
-/* PARAMÈTRE: indice - le numéro du tri à afficher (0 à NB_TRIS-1).  */
+/* Cette fonction affiche un point avec la "couleur" dÃ©sirÃ©e pour la */
+/* "valeur" Ã  la position de tableau "[pos]" du tri #indice_tri.     */
+/* PARAMÃˆTRE: indice - le numÃ©ro du tri Ã  afficher (0 Ã  NB_TRIS-1).  */
 /*            pos - position-tableau de la valeur (sur l'axe des X). */
-/*            valeur - la valeur à cette posiiton (sur l'axe des Y). */
-/*            taille - le nombre total d'éléments à trier.           */
-/*            couleur - la couleur de pixel désirée (entre 0 et 15). */
+/*            valeur - la valeur Ã  cette posiiton (sur l'axe des Y). */
+/*            taille - le nombre total d'Ã©lÃ©ments Ã  trier.           */
+/*            couleur - la couleur de pixel dÃ©sirÃ©e (entre 0 et 15). */
 /* ATTENTION : Le mode graphique doit avoir ete ouvert.			     */
-/* ATTENTION : Le cadre pour illustrer ce tri doit avoir été dessiné */
+/* ATTENTION : Le cadre pour illustrer ce tri doit avoir Ã©tÃ© dessinÃ© */
 /* ATTENTION : Elle supporte AU MAXIMUM 6 tris (indice < 6).         */
 void afficher_point(int indice, int pos, int valeur, int taille, int couleur);
 
-/* Permet de faire une pause de "msec" millisecondes à l'écran.      */
-/* PARAMÈTRE: msec - le temps de délai désiré en millisecondes.      */
+/* Permet de faire une pause de "msec" millisecondes Ã  l'Ã©cran.      */
+/* PARAMÃˆTRE: msec - le temps de dÃ©lai dÃ©sirÃ© en millisecondes.      */
 void delai_ecran(int msec);
 	
-/* Retourne 1 si une touche a été pesée, 0 sinon.                    */
+/* Retourne 1 si une touche a Ã©tÃ© pesÃ©e, 0 sinon.                    */
 int  touche_pesee();
 
-/* Pour récupérer un caractère saisi dans la console graphique.      */
+/* Pour rÃ©cupÃ©rer un caractÃ¨re saisi dans la console graphique.      */
 char obtenir_touche();
 	
 #endif
